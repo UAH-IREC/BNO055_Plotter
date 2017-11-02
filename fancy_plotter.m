6a1e611e01247c9626cdd644e35f86713b45981d
Thomas Bennett <trb0023@uah.edu>
2017-11-02 17:41:18 -0500

    Made it actually work
    Converted the values to work with our code instead of the arduino. I also found out that the orientation is World->Sensor and we need Sensor->World, so I changed the rotation function to use the inverse of our quaternion. I also added a statement to detect if the acceleration is pegged and discard the data if it is.

--- fancy_plotter.m
+++ fancy_plotter.m
@@ -1,5 +1,5 @@
 instrreset;
-s = serial('COM4');
+s = serial('COM34');
 s.baudrate = 115200;
 fopen(s);
 
@@ -12,19 +12,30 @@ while ishandle(fig)
     % Where w, x, y, z are the components of the quaternion
     % And X, Y, Z are the accelerations along the x, y, and z axes
     line = sscanf(fgets(s), '%g,', [7, 1]).';
-    disp(line);
+    %disp(line);
     if length(line) < 7 || all(line(1:4) == 0) 
         disp('Incomplete line');
         continue
     end
-    quat = line(1:4);
-    accel = line(5:7);
-    rot_accel = accel; %quatrotate(quat, accel);
+    
+
+    
+    quat = line(1:4) ./ 2^14;
+    accel = line(5:7) ./ 100;
+    
+    disp([quat accel]);
+    
+    if(max(abs(accel) > 300))
+        accel = [0 0 0];
+    end
+    
+    rot_accel = quatrotate(quatinv(quat), accel);
     this_vel = rot_accel * tstep;
     this_pos = this_vel * tstep;
     positions = [positions; positions(end, :) + this_pos];
     [rows, ~] = size(positions);
     plot(positions(:, 1), positions(:, 2));
+    axis vis3d
     drawnow;
     pause(0.001);
 end
